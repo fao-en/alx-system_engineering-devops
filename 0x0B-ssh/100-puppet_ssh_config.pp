9a7e04f638b2d402e3fa9dfde80a6966673c6568
fao-en <153403265+fao-en@users.noreply.github.com>
2024-01-19 14:07:04 +0100

    Add files via upload

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# File: 100-puppet_ssh_config.pp
+
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true,
+}
