fd399eb583f309e0669a3298bcb569d4c94be5d1
fao-en <fennaama@outlook.com>
2024-01-19 08:37:35 -0800

    insttaling some packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,21 @@
-# Install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install_python_flask_werkzeug.pp
+
+# Install Python 3
+package { 'python3':
+    ensure   => installed,
 }
+
+# Install Flask using pip3
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Package['python3'],
+}
+
+# Install Werkzeug using pip3
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+    require  => Package['flask'],
+}
+
