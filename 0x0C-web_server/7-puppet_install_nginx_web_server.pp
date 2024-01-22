bae1043d95e9049d8336ec45947377a933849a79
fao-en <fennaama@outlook.com>
2024-01-22 08:53:46 -0800

    web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# Puppet manifest to install nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'redirect_me_config':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+  notify => Service['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
